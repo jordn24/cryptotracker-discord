4c6e8b62e6c4676b9aac9db270576dd95f4da03b
Jordan <jordan.sposito2430@gmail.com>
2021-06-26 13:12:38 +1000

    Added emed messages and cleaned up text

--- main.js
+++ main.js
@@ -7,6 +7,30 @@ const client = new discord.Client();
 const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 
+const green = '#33FF4C'
+const red = '#FF3333'
+
+function formatMoney(number) {
+	return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+}
+
+function printEmbed(title, fields, image) {
+
+	// Green or red colour
+	if(fields[2].value > 0){
+		colour = green
+	} else {
+		colour = red
+	}
+	fields[2].value = fields[2].value + "%"
+
+	return embed = new discord.MessageEmbed()
+				.setColor(colour)
+				.setTitle(title)
+				.addFields(fields)
+				.setImage(image)
+}
+
 client.on('message', async message => {
 
 	// HELP COMMAND
@@ -39,13 +63,52 @@ client.on('message', async message => {
 			
 			// Retrieve Data
 			data = await CoinGeckoClient.coins.fetch(coinId, {});
-			message.channel.send(coinName.toUpperCase())
-			message.channel.send("AUD: $" + data["data"]["market_data"]["current_price"]["aud"])
-			message.channel.send("USD: $" + data["data"]["market_data"]["current_price"]["usd"])
-			message.channel.send("24 Hour Change: " + data["data"]["market_data"]["price_change_percentage_24h"] +
-				"%")
-			
+
+			// Send embed
+			fields = [
+				{ name:"$AUD" ,value: "$" + formatMoney(data["data"]["market_data"]["current_price"]["aud"])},
+				{ name:"$USD" ,value: "$" + formatMoney(data["data"]["market_data"]["current_price"]["usd"])},
+				{ name:"% Difference" ,value: data["data"]["market_data"]["price_change_percentage_24h"]}
+			]
+			message.channel.send(printEmbed(coinName.toUpperCase(), fields, data["data"]["image"]["large"]))
+
 			return
+		}
+
+	// RANDOM COMMANDS
+		if (message.content.toLowerCase().includes(prefix + 'random ')){
+			
+			if (message.content.toLowerCase().includes('crypto')){
+				// Get list of coins
+				data = await CoinGeckoClient.coins.list();
+				length = data["data"].length
+
+				// Random number within length
+				i = Math.floor(Math.random() * length);
+
+				// Find corresponding
+				data["data"][i]["symbol"]
+				coinId = data["data"][i]["id"]
+				coinName = data["data"][i]["name"]
+
+				// Retrieve Data
+				data = await CoinGeckoClient.coins.fetch(coinId, {});
+
+				// Send embed
+				fields = [
+					{ name:"$AUD" ,value: "$" + formatMoney(data["data"]["market_data"]["current_price"]["aud"])} ,
+					{ name:"$USD" ,value: "$" + formatMoney(data["data"]["market_data"]["current_price"]["usd"])} ,
+					{ name:"% Difference" ,value: data["data"]["market_data"]["price_change_percentage_24h"] + "%" }
+				]
+				message.channel.send(printEmbed(coinName.toUpperCase(), fields, data["data"]["image"]["large"]))
+				return
+		
+			}
+
+			if (message.content.toLowerCase().includes('stock')){
+				message.channel.send("Not working yet.")
+			}
+
 		}
 
 	// STOCK DATA COMMAND
